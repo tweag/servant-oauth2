790b10a36ab1e680ea3189ed6065702d07ad4b8e
silky <noonsilk+-noonsilk@gmail.com>
2022-07-09 12:21:14 +0100

    Simpler approach - just use AuthProtect
    The key assumption is that we can _just_ implement the "complete" step,
    and ask the caller to work out how to navigate to the login pages; I
    think this works.
    
    - We just use `AuthProtect`
    - Move most of the login to the `Servant.OAuth2` module
    - Allow some configuration

--- example/Main.hs
+++ example/Main.hs
@@ -8,58 +8,57 @@ import "text" Data.Text (Text)
 import "base" GHC.Generics (Generic)
 import "wai" Network.Wai (Request)
 import "warp" Network.Wai.Handler.Warp (run)
-import "servant-server" Servant (Context (EmptyContext, (:.)), Get, Handler, NamedRoutes, type (:>))
+import "servant-server" Servant
+  ( AuthProtect,
+    Context (EmptyContext, (:.)),
+    Get,
+    Handler,
+    NamedRoutes,
+    type (:>),
+  )
 import "servant" Servant.API.Generic ((:-))
 import "servant-blaze" Servant.HTML.Blaze (HTML)
-import "servant-server" Servant.Server.Generic (AsServerT, genericServeTWithContext)
-import "shakespeare" Text.Hamlet (Html, shamlet)
-
 import Servant.OAuth2
-
+import "servant-server" Servant.Server.Experimental.Auth
+  ( AuthHandler,
+    AuthServerData,
+  )
+import "servant-server" Servant.Server.Generic
+  ( AsServerT,
+    genericServeTWithContext,
+  )
+import "shakespeare" Text.Hamlet (Html, shamlet)
+import "cookie" Web.Cookie (SetCookie)
+import Data.Text.Encoding (decodeUtf8)
 
 type PageM = Handler
-
 
 data Session = Session
   { ident :: Text
   }
 
-
-type instance OAuth2ProviderData (OAuth2Provider GitHubProvider) = Session
-
-
-f :: OAuth2Handler Request Session
-f = undefined
-
-
 data Routes mode = Routes
-  { home :: mode :- Get '[HTML] Html
-  , admin :: mode :- "admin" :> NamedRoutes AdminRoutes
-  , auth :: mode :- OAuth2Provider GitHubProvider :> NamedRoutes AuthRoutes
+  { home :: mode :- Get '[HTML] Html,
+    admin :: mode :- "admin" :> NamedRoutes AdminRoutes,
+    auth :: mode :- AuthProtect "oauth2" :> NamedRoutes (OAuth2Routes OAuth2Result)
   }
   deriving stock (Generic)
-
-
-data AuthRoutes mode = AuthRoutes
-  { complete :: mode :- Get '[HTML] Html
-  }
-  deriving stock (Generic)
-
 
 data AdminRoutes mode = AdminRoutes
   { adminHome :: mode :- Get '[HTML] Html
   }
   deriving stock (Generic)
 
+type OAuth2Result = Text
+settings = defaultOAuth2Settings { success = pure . decodeUtf8 }
 
 server :: Routes (AsServerT PageM)
 server =
   Routes
-    { home = pure $ [shamlet| <p> Home |]
-    , admin = adminServer
-    , auth = undefined
+    { home = pure $ [shamlet| <p> Home |],
+      admin = adminServer,
+      auth = authServer @OAuth2Result settings
     }
-
 
 adminServer :: AdminRoutes (AsServerT PageM)
 adminServer =
@@ -72,11 +71,11 @@ adminServer =
       |]
     }
 
-
 main :: IO ()
 main = do
   run 8080 $
     genericServeTWithContext nat server context
   where
-    context = f :. EmptyContext
+    -- context :: Context '[AuthHandler Request SetCookie]
+    context = oauth2AuthHandler :. EmptyContext
     nat = id
--- src/Servant/OAuth2.hs
+++ src/Servant/OAuth2.hs
@@ -7,48 +7,92 @@
 module Servant.OAuth2 where
 
 import "base" Control.Monad.IO.Class (liftIO)
+import "bytestring" Data.ByteString
 import "base" Data.Kind (Type)
+import "text" Data.Text (Text)
 import "base" Data.Typeable (Typeable)
+import "base" GHC.Generics (Generic)
 import "wai" Network.Wai (Request)
-import "servant-server" Servant (Handler, HasContextEntry, HasServer (..), Proxy (Proxy), type (:>))
-import "servant-server" Servant.Server.Internal (DelayedIO, addAuthCheck, delayedFailFatal, getContextEntry, runHandler, withRequest)
+import "servant-server" Servant
+  ( AuthProtect,
+    Get,
+    Handler,
+    HasContextEntry,
+    HasServer (..),
+    Header,
+    Headers,
+    NoContent (NoContent),
+    Proxy (Proxy),
+    addHeader,
+    type (:>),
+  )
+import "servant" Servant.API.Generic ((:-))
+import "servant-blaze" Servant.HTML.Blaze (HTML)
+import "servant-server" Servant.Server.Experimental.Auth
+  ( AuthHandler,
+    AuthServerData,
+  )
+import "servant-server" Servant.Server.Generic
+  ( AsServerT,
+  )
+import "servant-server" Servant.Server.Internal
+  ( DelayedIO,
+    addAuthCheck,
+    delayedFailFatal,
+    getContextEntry,
+    runHandler,
+    withRequest,
+  )
+import "cookie" Web.Cookie (SetCookie)
 
+type Ident = ByteString
 
-data GitHubProvider
-data GoogleProvider
-data GitLabProvider
+-- | We begin
+type instance AuthServerData (AuthProtect "oauth2") = Ident
 
+-- | Then, let's begin.
+type Redirect =
+  Headers '[Header "Location" Text] NoContent
 
-data OAuth2Provider (tag :: k) deriving (Typeable)
+redirect :: Text -> Redirect
+redirect location = addHeader location NoContent
 
+type RedirectWithCookie =
+  Headers '[Header "Location" Text, Header "Set-Cookie" SetCookie] NoContent
 
-type family OAuth2ProviderData (a :: Type)
+redirectWithCookie ::
+  Text ->
+  SetCookie ->
+  RedirectWithCookie
+redirectWithCookie destination cookie =
+  addHeader destination (addHeader cookie NoContent)
 
+data OAuth2Routes a mode = AuthRoutes
+  { complete :: mode :- Get '[HTML] a
+  }
+  deriving stock (Generic)
 
-newtype OAuth2Handler r usr = OAuth2Handler
-  { unOAuth2Handler :: r -> Handler usr
+authServer :: forall a. OAuth2Settings a -> Ident -> OAuth2Routes a (AsServerT Handler)
+authServer settings ident =
+  AuthRoutes
+    { complete = success settings $ ident
+    }
+
+oauth2AuthHandler :: AuthHandler Request Ident
+oauth2AuthHandler = undefined
+
+data OAuth2Settings a = OAuth2Settings
+  { success :: Ident -> Handler a
   }
 
-
-instance
-  ( HasServer api context
-  , HasContextEntry context (OAuth2Handler Request (OAuth2ProviderData (OAuth2Provider tag)))
-  ) =>
-  HasServer (OAuth2Provider tag :> api) context
+defaultOAuth2Settings :: OAuth2Settings RedirectWithCookie
+defaultOAuth2Settings =
+  OAuth2Settings
+    { success = \ident -> do
+        cookie <- buildSessionCookie key ident
+        pure $ redirectWithCookie "/" cookie
+    }
   where
-  type
-    ServerT (OAuth2Provider tag :> api) m =
-      OAuth2ProviderData (OAuth2Provider tag) -> ServerT api m
+    key = undefined
 
-
-  hoistServerWithContext _ pc nt s = hoistServerWithContext (Proxy :: Proxy api) pc nt . s
-
-
-  route Proxy context subserver =
-    route (Proxy :: Proxy api) context (subserver `addAuthCheck` withRequest authCheck)
-    where
-      authHandler :: Request -> Handler (OAuth2ProviderData (OAuth2Provider tag))
-      authHandler = unOAuth2Handler (getContextEntry context)
-
-      authCheck :: Request -> DelayedIO (OAuth2ProviderData (OAuth2Provider tag))
-      authCheck = (>>= either delayedFailFatal return) . liftIO . runHandler . authHandler
+buildSessionCookie = undefined
