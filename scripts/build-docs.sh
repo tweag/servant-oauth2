80b83d6bf2e8b0d15c2c084ee39fe5c01f092644
Noon van der Silk <noon.vandersilk@tweag.io>
2022-07-18 14:01:33 +0100

    docs (#22)

---
+++ scripts/build-docs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+mkdir -p docs/servant-oauth2
+mkdir -p docs/servant-oauth2-examples
+
+pname=`cat package.yaml | grep "name:" | awk '{ print $2 }'`
+ver=`cat package.yaml | grep "version:" | awk '{ print $2 }'`
+stack haddock --no-haddock-deps --fast
+doc_root=`stack path --local-doc-root`/$pname-$ver
+
+cp -r $doc_root/* docs/servant-oauth2
+
+
+cd servant-oauth2-examples
+pname=`cat package.yaml | grep "name:" | awk '{ print $2 }'`
+ver=`cat package.yaml | grep "version:" | awk '{ print $2 }'`
+stack haddock --no-haddock-deps --fast
+doc_root=`stack path --local-doc-root`/$pname-$ver
+
+cp -r $doc_root/* ../docs/servant-oauth2-examples
