50b70b31a20c8b3adfdaaaa87fdd3b6eced36b29
Noon van der Silk <noon.vandersilk@tweag.io>
2022-07-19 12:46:31 +0100

    correct links for additional packages

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -14,3 +14,8 @@ doc_root=$(dirname "$index")
 
 cp -r $doc_root/servant-oauth2-$ver/* docs/servant-oauth2
 cp -r $doc_root/servant-oauth2-examples-$exVer/* docs/servant-oauth2-examples
+
+# Elegant method of making the links correct ...
+# Note: lts-19.14 is hard-coded here. Could locate it from the `stack.yaml` if
+# we wished it.
+sed -i 's/href=\"\.\./\href="\https:\/\/www\.stackage\.org\/haddock\/lts-19\.14/g' docs/**/*.html
